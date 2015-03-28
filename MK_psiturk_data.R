1cf3f4b35d84e98f297395be5979a19726216f53
MASM5 <masm@mit.edu>
2015-03-28 12:45:20 -0400

    Updated Analysis Script
    Added MK_psiturk_data to analyze data from Turk

--- MK_psiturk_data.R
+++ MK_psiturk_data.R
@@ -15,25 +15,23 @@ stderr <- function(x) sqrt(var(x)/length(x))
 
 # Read data ---------------------------------------------------------------
 
-con = dbConnect(SQLite(),dbname = "many-dax-jspsych-psiturk-experiment_1_13_15/participants.db");
-df.complete = dbReadTable(con,"turkdemo") #change the name of the database here (mine was called "almost")
+con = dbConnect(SQLite(),dbname = "/Users/miguelsalinas/Desktop/GlyphsTurk/participants.db");
+df.complete = dbReadTable(con,"glyphs") #change the name of the database here (mine was called "almost")
 dbDisconnect(con)
 
 #filter out incompletes (using dplyr methods)
-df.complete = subset(df.complete,status %in% c(3,4)) 
+df.complete = subset(df.complete, status %in% c(3,4)) 
 
-nrow(df.complete) #includes alll subjects ever plus all debug attempts!
+#nrow(df.complete) includes alll subjects ever plus all debug attempts!
 #filter to a particular day (if I haven't set codeversions). OR together multiple days if needed
-df.complete$currentVersion1 = str_detect(df.complete$beginhit, "2015-01-13")
-df.complete$currentVersion2 = str_detect(df.complete$beginhit, "2015-01-14")
-df.complete$currentVersion3 = str_detect(df.complete$beginhit, "2015-01-15")
-df.complete$currentVersion4 = str_detect(df.complete$beginhit, "2015-01-16")
+df.complete$currentVersion.pilot1 = str_detect(df.complete$beginhit, "2015-03-24")
+df.complete$currentVersion.pilot2 = str_detect(df.complete$beginhit, "2015-03-25")
 
-#Run 1, 1/13/15-1/15/15
-#df.complete = df.complete[df.complete$currentVersion1 == TRUE | df.complete$currentVersion2 == TRUE |df.complete$currentVersion3 == TRUE,]
+#Run 1, 03/24/2015 - 03/25/2015
+df.complete = df.complete[df.complete$currentVersion.pilot1 == TRUE | df.complete$currentVersion.pilot2 == TRUE,]
 
 #Run 2, 1/16/15
-df.complete = df.complete[df.complete$currentVersion4 == TRUE,]
+#df.complete = df.complete[df.complete$currentVersion4 == TRUE,]
 
 nrow(df.complete)
 
@@ -48,7 +46,12 @@ nrow(df.complete)
 df.wide = data.frame(matrix(nrow=nrow(df.complete),ncol=4))
 colnames(df.wide) = c("participant","workerId","browser","beginhit") #will dynamically add columns from datastring below
 
+global_indeces = c()
+free_sorts = list()
+
+
 for (i in 1:nrow(df.wide)){
+  partic_free = list()
   if (!is.na(df.complete$datastring[i])){
     a = fromJSON(df.complete$datastring[i])
     mylength = length(a$data)
@@ -57,32 +60,158 @@ for (i in 1:nrow(df.wide)){
     mylength = 0
   }
   print(mylength)
-  if (mylength==27){
+  if (mylength>=85){
     df.wide$participant[i] = i
     df.wide$workerId[i] = a$workerId
     df.wide$browser[i] = df.complete$browser[i]
     df.wide$beginhit[i] = df.complete$beginhit[i]
     #cycle through all the trials, but only record where isTestTrial = 1
     for (j in 1:mylength){
-      if(a$data[[j]]$trialdata$isTestTrial == "1"){
-        df.wide[[paste("rt_",j, sep="")]][i] = a$data[[j]]$trialdata$rt
-        df.wide[[paste("keypress_",j, sep="")]][i] = a$data[[j]]$trialdata$key_press
-        df.wide[[paste("stimCondition_",j, sep="")]][i] = a$data[[j]]$trialdata$stimCondition
-        df.wide[[paste("stimName_",j, sep="")]][i] = a$data[[j]]$trialdata$stimName
-        df.wide[[paste("exposurePath_",j, sep="")]][i] = a$data[[j]]$trialdata$exposure_path
-        df.wide[[paste("exposureManner_",j, sep="")]][i] = a$data[[j]]$trialdata$exposure_manner
-        df.wide[[paste("condition_",j, sep="")]][i] = a$data[[j]]$trialdata$condition_name
+      if(a$data[[j]]$trialdata$trial_type == "free-sort"){
+        partic_free = c(partic_free, list(a$data[[j]]$trialdata))
       } #Else just don't make any columns right now!!!
     }
-  }
+    free_sorts[a$workerId] = list(partic_free)
+  } 
+}
 
-  #And grab the info we need from the last 'trial' (feedback)
-  if (is.null(a$data[[mylength-1]]$trialdata$responses)){df.wide$feedback[i] = "none"
-  }else{
-    df.wide$feedback[i] = a$data[[mylength-1]]$trialdata$responses
+name_it = names(free_sorts)
+for (i in 1:length(free_sorts)) {
+  if (length(free_sorts[[name_it[i]]]) != 60 | i==11 | i==14) {
+   df.wide[i,] = 'EXCLUDED' 
+   df.wide$workerId[i] = 'TOO MANY TRIALS'
   }
+}
+
+for (i in 1:nrow(df.wide)){
+  if (df.wide$participant[i] != 'EXCLUDED'){
+    a = free_sorts[[df.wide$workerId[i]]]
+    mylength = length(free_sorts[[df.wide$workerId[i]]])
+  } else{
+    a = data.frame(NULL)
+    mylength = 0
+    df.wide[i,] = 'EXCLUDED' 
+    df.wide$workerId[i] = 'TOO MANY TRIALS'
+  }
+  for (j in 1:mylength) {
+  if(!is.null(free_sorts[[df.wide$workerId[i]]][[j]]$rt)){
+    df.wide[[paste("rt_",free_sorts[[df.wide$workerId[i]]][[j]]$trial_index_global, sep="")]][i] = free_sorts[[df.wide$workerId[i]]][[j]]$rt
+  }
+  if(!is.null(free_sorts[[df.wide$workerId[i]]][[j]]$moves)){
+    df.wide[[paste("moves_",free_sorts[[df.wide$workerId[i]]][[j]]$trial_index_global, sep="")]][i] = free_sorts[[df.wide$workerId[i]]][[j]]$moves
+    if (free_sorts[[df.wide$workerId[i]]][[j]]$trial_index_global > 23) {
+      global_indeces <- unique(c(global_indeces,free_sorts[[df.wide$workerId[i]]][[j]]$trial_index_global))
+    }
+  }
+  if(!is.null(free_sorts[[df.wide$workerId[i]]][[j]]$glyph)){
+    df.wide[[paste("glyph_",free_sorts[[df.wide$workerId[i]]][[j]]$trial_index_global, sep="")]][i] = free_sorts[[df.wide$workerId[i]]][[j]]$glyph
+  }
+  if(!is.null(free_sorts[[df.wide$workerId[i]]][[j]]$Sub)){
+    df.wide[[paste("S_",free_sorts[[df.wide$workerId[i]]][[j]]$trial_index_global, sep="")]][i] = free_sorts[[df.wide$workerId[i]]][[j]]$Sub
+  }
+  if(!is.null(free_sorts[[df.wide$workerId[i]]][[j]]$Vrb)){
+    df.wide[[paste("V_",free_sorts[[df.wide$workerId[i]]][[j]]$trial_index_global, sep="")]][i] = free_sorts[[df.wide$workerId[i]]][[j]]$Vrb
+  }
+  if(!is.null(free_sorts[[df.wide$workerId[i]]][[j]]$Obj)){
+    df.wide[[paste("O_",free_sorts[[df.wide$workerId[i]]][[j]]$trial_index_global, sep="")]][i] = free_sorts[[df.wide$workerId[i]]][[j]]$Obj
+  }
+  if(!is.null(free_sorts[[df.wide$workerId[i]]][[j]]$moviefile)){
+    df.wide[[paste("MovieFile_",free_sorts[[df.wide$workerId[i]]][[j]]$trial_index_global, sep="")]][i] = free_sorts[[df.wide$workerId[i]]][[j]]$moviefile
+  }
+  }
+}
+names = c('moves_', "S_", "O_", "V_")
+
+for (w in 1:length(global_indeces)) {
+  df.wide[paste('WordOrder_', as.character(global_indeces[w]), sep = "")] = "EXCLUDED"
+}
+
+for (j in 1:nrow(df.wide)){
+  if (df.wide$participant[j] != "EXCLUDED"){  
+    for (i in 2:length(names)){
+      for (k in 1:length(global_indeces)) {
+        check_m = df.wide[paste(names[1], as.character(global_indeces[k]), sep = "")][j,]
+        check_s = df.wide[paste(names[2], as.character(global_indeces[k]), sep = "")][j,]
+        check_o = df.wide[paste(names[3], as.character(global_indeces[k]), sep = "")][j,]
+        check_v = df.wide[paste(names[4], as.character(global_indeces[k]), sep = "")][j,]
+        where_s = unlist(gregexpr(check_s, check_m))
+        where_o = unlist(gregexpr(check_o, check_m))
+        where_v = unlist(gregexpr(check_v, check_m))
+        where_svo = c(where_s, where_v, where_o)
+        where_svo = sort(where_svo)
+        word_order = ''
+        for (l in 1:length(where_svo)) {
+          if (where_svo[l] %in% where_s) {
+            word_order = paste(word_order, 'S', sep='')
+          } else if (where_svo[l] %in% where_o) {
+            word_order = paste(word_order, 'O', sep='')
+          } else if (where_svo[l] %in% where_v) {
+            word_order = paste(word_order, 'V', sep='')
+          } 
+        }
+        df.wide[paste('WordOrder_', as.character(global_indeces[k]), sep = "")][j,] = word_order
+      }
+    }
+  }
+}
+
+#   #And grab the info we need from the last 'trial' (feedback)
+#   if (is.null(a$data[[mylength-1]]$trialdata$responses)){df.wide$feedback[i] = "none"
+#   }else{
+#     df.wide$feedback[i] = a$data[[mylength-1]]$trialdata$responses
+#   }
 } #End of this participant
 
+# Run this again for participants who did not pass the quiz the first time.
+# for (i in 1:nrow(df.wide)){
+#   if (!is.na(df.complete$datastring[i])){
+#     a = fromJSON(df.complete$datastring[i])
+#     mylength = length(a$data)
+#   } else{
+#     a = data.frame(NULL)
+#     mylength = 0
+#   }
+#   print(mylength)
+#   if (mylength>86){
+#     df.wide$participant[i] = i
+#     df.wide$workerId[i] = a$workerId
+#     df.wide$browser[i] = df.complete$browser[i]
+#     df.wide$beginhit[i] = df.complete$beginhit[i]
+#     for (j in 1:mylength){
+#       try(
+#       if(a$data[[j]]$trialdata$trial_index_global %in% global_indeces){
+#         if(!is.null(a$data[[j]]$trialdata$rt)){
+#           df.wide[[paste("rt_",a$data[[j]]$trialdata$trial_index_global, sep="")]][i] = a$data[[j]]$trialdata$rt
+#         }
+#         if(!is.null(a$data[[j]]$trialdata$moves)){
+#           df.wide[[paste("moves_",a$data[[j]]$trialdata$trial_index_global, sep="")]][i] = a$data[[j]]$trialdata$moves
+#           global_indeces <- unique(c(global_indeces,a$data[[j]]$trialdata$trial_index_global))
+#         }
+#         if(!is.null(a$data[[j]]$trialdata$glyph)){
+#           df.wide[[paste("PracGlyph_",a$data[[j]]$trialdata$trial_index_global, sep="")]][i] = a$data[[j]]$trialdata$glyph
+#         }
+#         if(!is.null(a$data[[j]]$trialdata$Sub)){
+#           df.wide[[paste("Sub_",a$data[[j]]$trialdata$trial_index_global, sep="")]][i] = a$data[[j]]$trialdata$Sub
+#         }
+#         if(!is.null(a$data[[j]]$trialdata$Vrb)){
+#           df.wide[[paste("Vrb_",a$data[[j]]$trialdata$trial_index_global, sep="")]][i] = a$data[[j]]$trialdata$Vrb
+#         }
+#         if(!is.null(a$data[[j]]$trialdata$Obj)){
+#           df.wide[[paste("Obj_",a$data[[j]]$trialdata$trial_index_global, sep="")]][i] = a$data[[j]]$trialdata$Obj
+#         }
+#       } #Else just don't make any columns right now!!!
+#     )}
+#   }
+#   
+#   #And grab the info we need from the last 'trial' (feedback)
+#   if (is.null(a$data[[mylength-1]]$trialdata$responses)){df.wide$feedback[i] = "none"
+#   }else{
+#     df.wide$feedback[i] = a$data[[mylength-1]]$trialdata$responses
+#   }
+# } 
+
+    
+    
 #Notes: Something up with 1/16/15 subj 71's datastring:it's missing - recorded at end, so presumably lost before that?  Add a check for null datastrings
 
 #Weird behavior! I got those wrong-lenght participants to be assigned a participant no of NA, which is something, anyway.
